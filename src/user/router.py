90a199db60e33e81151a469d4363fcdb80cbfda6
memgraal <123896677+memgraal@users.noreply.github.com>
2025-08-11 22:43:26 +0300

    начало создания пакеты user

---
+++ src/user/router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from auth.config import security
+
+
+UserRouter =APIRouter(
+    prefix="/user",
+    tags=['UserProfile'],   
+)
+
+
+@UserRouter.get("/profile", dependencies=[Depends(security.get_token_from_request)])
+async def get_user_profile(token: str = Depends()) -> dict:
+    try:
+        security.verify_token(token)
+        return {"message": "User profile data"} # временный пример (сделать после слияния ветки feaature-database и user-registration)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    
